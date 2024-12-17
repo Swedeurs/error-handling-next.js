7cc88eb73a9ccf57c95046d59ab8dcd0c03699ab
CarlUdo <carl@wbsweden.com>
2024-12-17 13:55:57 +0100

    Run-prettier
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/dashboard/actions.ts
+++ src/features/dashboard/actions.ts
@@ -3,12 +3,12 @@
 import { revalidatePath } from "next/cache";
 import { dashboardService } from "./instance";
 
-export const onButtonClickAction = async (formData: FormData) => {    
-    const name = formData.get("name") as string;
-    try {
-        await dashboardService.addName(name)
-    } catch (error) {
-        throw new Error("Something went wrong again" + error);
-    }
+export const onButtonClickAction = async (formData: FormData) => {
+  const name = formData.get("name") as string;
+  try {
+    await dashboardService.addName(name);
+  } catch (error) {
+    throw new Error("Something went wrong again" + error);
+  }
   revalidatePath("/dashboard");
 };
