2220923e1e19c1703becb5309125d6cf35edeea5
CarlUdo <carl@wbsweden.com>
2024-12-17 14:35:28 +0100

    Change-text
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/dashboard/actions.ts
+++ src/features/dashboard/actions.ts
@@ -8,7 +8,7 @@ export const onButtonClickAction = async (formData: FormData) => {
   try {
     await dashboardService.addName(name);
   } catch (error) {
-    throw new Error("Something went wrong again" + error);
+    throw new Error("Forward error" + error);
   }
   revalidatePath("/dashboard");
 };
