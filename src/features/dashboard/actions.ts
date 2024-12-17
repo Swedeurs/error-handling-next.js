861188b683473c1e9f510e3f822e94f4ace34792
CarlUdo <carl@wbsweden.com>
2024-12-17 13:50:48 +0100

    Remove-console-logs
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/dashboard/actions.ts
+++ src/features/dashboard/actions.ts
@@ -3,13 +3,11 @@
 import { revalidatePath } from "next/cache";
 import { dashboardService } from "./instance";
 
-export const onButtonClickAction = async (formData: FormData) => {
+export const onButtonClickAction = async (formData: FormData) => {    
     const name = formData.get("name") as string;
     try {
-        dashboardService.addName(name);
-        throw new Error("Something went wrong");
+        await dashboardService.addName(name)
     } catch (error) {
-        console.error("fångar fel" + error);
         throw new Error("Something went wrong again" + error);
     }
   revalidatePath("/dashboard");
