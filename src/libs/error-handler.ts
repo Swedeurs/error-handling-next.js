0011da3fdf80f1f636709867f2eeb32d4adb7651
CarlUdo <carl@wbsweden.com>
2024-12-19 11:08:41 +0100

    Make-error-handler-function
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

---
+++ src/libs/error-handler.ts
@@ -0,0 +1,40 @@
+import { ForbiddenError, NotFoundError, UnauthorizedError } from "./errors";
+import { ZodError } from "zod";
+
+type CustomError =
+  | Error
+  | UnauthorizedError
+  | ForbiddenError
+  | NotFoundError
+  | unknown;
+
+export const errorHandler = (error: CustomError) => {
+  let errorText =
+    `Title: Internal server error\n` +
+    `Status code: 500\n` +
+    `Message: Something went wrong`;
+  if (
+    error instanceof UnauthorizedError ||
+    error instanceof ForbiddenError ||
+    error instanceof NotFoundError
+  ) {
+    errorText =
+      `Title: ${error.title}\n` +
+      `Status code: ${error.statusCode}\n` +
+      `Message: ${error.message}`;
+  } else if (error instanceof ZodError) {
+    const jsonArray = JSON.parse(error.message);
+    const message = jsonArray[0].message;
+    errorText =
+      `Title: Validation error\n` +
+      `Status code: 400\n` +
+      `Message: ${message}`;
+  } else if (error instanceof Error) {
+    errorText =
+      `Title: App error\n` + 
+      `Status code: 500\n` + 
+      `Message: ${error.message}`;
+  }
+
+  throw new Error(errorText);
+};
