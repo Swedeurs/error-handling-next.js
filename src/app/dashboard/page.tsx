95d7ba2a7d17d29b162ca3a22c514393320db1ed
CarlUdo <carl@wbsweden.com>
2024-12-19 11:09:42 +0100

    Remove-files
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-import { DashboardForm } from "@/features/dashboard/components/dashboard-form";
-
-export default function Page() {
-  return <DashboardForm />;
-}
--- src/features/dashboard/instance.ts
+++
@@ -1,4 +0,0 @@
-import { db } from "@/db/db";
-import { createService } from "./service";
-
-export const dashboardService = createService(db);
